c462eadb7087cef015b877ac69bba0ba8d2919f8
aakasyadav-sys <aakasyadav@sysconinfotech.com>
2025-11-12 19:12:10 +0530

    updated logout Api

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,13 +1,11 @@
 import { Router } from "express";
 import { loginUser, logoutUser } from "../controller/userController.js";
 import { getRole } from "../controller/userTypeController.js";
-import { getAllUsers } from "../controller/getAllUserController.js";
+import { authenticateJWT } from "../middleware/authenticateJWT.js";
 
 const router = Router();
 router.get("/login", loginUser);
 router.get("/role", getRole);
-router.get("/", getAllUsers);
-router.post("/logout", logoutUser);
-
+router.post("/logout", authenticateJWT, logoutUser);
 
 export default router;
