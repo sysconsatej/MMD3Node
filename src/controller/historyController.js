7ebd86c8f7974a04b3932af70edaa7cf67b593d8
Abdullah Ansari <abdullah.sysconinfotech@gmail.com>
2025-12-06 17:03:23 +0530

    hbl highlight columns functionality

--- src/controller/historyController.js
+++ src/controller/historyController.js
@@ -43,6 +43,38 @@ export const getHistoryData = async (req, res) => {
     });
   } catch (error) {
     console.log("Error fetching history data:", error);
-    return res.status(500).json({ success: false, message: "Internal server error" });
+    return res
+      .status(500)
+      .json({ success: false, message: "Internal server error" });
+  }
+};
+
+export const getHblColumnChanges = async (req, res) => {
+  const { ids } = req.body;
+
+  if (!ids) {
+    return res.status(400).json({ message: "The 'ids' parameter is required" });
+  }
+
+  try {
+    const query = `EXEC hblColumnChange @ids = @ids`;
+
+    const parameters = { ids };
+
+    const result = await executeQuery(query, parameters);
+    const jsonStr = Object.values(result[0])[0];
+    const parsed = JSON.parse(jsonStr);
+
+    res.status(200).json({
+      success: true,
+      message: "Successfully fetched data",
+      data: parsed,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: "Error executing getDataApi API",
+      error: err.message,
+    });
   }
 };
--- src/routes/historyRoute.js
+++ src/routes/historyRoute.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { getHistoryData } from "../controller/historyController.js";
+import {
+  getHblColumnChanges,
+  getHistoryData,
+} from "../controller/historyController.js";
 
 const router = express.Router();
 
 // GET /api/history?tableName=tblBl&id=6283
 router.get("/history", getHistoryData);
+router.post("/getHblColumnChanges", getHblColumnChanges);
 
 export default router;
